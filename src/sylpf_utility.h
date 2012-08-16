a091e8387e72a4985a2ab06e94d5c7b67b07cf06
HAYASHI Kentaro <kenhys@gmail.com>
2012-08-17 00:05:16 +0900

    add SYLPF_SYLPHEEDRC

--- src/sylpf_utility.h
+++ src/sylpf_utility.h
@@ -1,6 +1,8 @@
 #ifndef __SYLPF_UTILITY_H_INCLUDED__
 #define __SYLPF_UTILITY_H_INCLUDED__
 
+#define SYLPF_SYLPHEEDRC "sylpheedrc"
+
 #define GET_RC_INT(keyfile, group, key) \
   g_key_file_get_integer((keyfile), (group), (key), NULL)
 
