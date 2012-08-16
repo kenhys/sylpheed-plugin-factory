9f6958ca2f56e7c07c771ec98d71f4ecfe36280e
HAYASHI Kentaro <kenhys@gmail.com>
2012-08-16 23:18:14 +0900

    define SYLPF_GET/SET_RC_BOOLEAN

--- src/sylpf_utility.h
+++ src/sylpf_utility.h
@@ -1,2 +1,13 @@
+#ifndef __SYLPF_UTILITY_H_INCLUDED__
+#define __SYLPF_UTILITY_H_INCLUDED__
+
 #define GET_RC_INT(keyfile, group, key) \
   g_key_file_get_integer((keyfile), (group), (key), NULL)
+
+#define SYLPF_GET_RC_BOOLEAN(keyarg) \
+  g_key_file_get_boolean(SYLPF_OPTION.rcfile, SYLPF_ID, keyarg, NULL)
+
+#define SYLPF_SET_RC_BOOLEAN(key, value) \
+  g_key_file_set_boolean(SYLPF_OPTION.rcfile, SYLPF_ID, key, value)
+
+#endif
