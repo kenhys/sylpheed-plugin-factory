c78729dc9d5e0dfd1c93b78e201baa43e3f8dde4
HAYASHI Kentaro <kenhys@gmail.com>
2013-04-03 23:54:00 +0900

    add travis setup script for sylpheed

---
+++ misc/setup-travis.sh
@@ -0,0 +1,18 @@
+
+
+case "$SYLPHEED_STAGE" in
+    master|head)
+	svn checkout svn://sylpheed.sraoss.jp/sylpheed/trunk sylpheed
+	(cd sylpheed; ./autogen.sh; ./configure --prefix=/usr; make; sudo make install)
+	;;
+    3.4.0|3.4|beta)
+	wget http://sylpheed.sraoss.jp/sylpheed/v3.4beta/sylpheed-3.4.0beta3.tar.bz2
+	tar ixf sylpheed-3.4.0beta3.tar.bz2
+	(cd sylpheed-3.4.0beta3; ./configure --prefix=/usr; make; sudo make install)
+	;;
+    3.3.0|3.3|stable|*)
+	wget http://sylpheed.sraoss.jp/sylpheed/v3.3/sylpheed-3.3.0.tar.bz2
+	tar ixf sylpheed-3.3.0.tar.bz2
+	(cd sylpheed-3.3.0; ./configure --prefix=/usr; make; sudo make install)
+	;;
+esac
