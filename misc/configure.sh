df1e0dafb083ef7c9063c5971e7f78ab430b6d73
HAYASHI Kentaro <kenhys@gmail.com>
2013-04-14 01:31:13 +0900

    add configure script

---
+++ misc/configure.sh
@@ -0,0 +1,10 @@
+OPTIONS=""
+
+if [ -n "$USE_WEBKITGTK" ]; then
+    OPTIONS="$OPTIONS --enable-webkitgtk"
+fi
+if [ -n "$USE_GTKHTML" ]; then
+    OPTIONS="$OPTIONS --enable-gtkhtml"
+fi
+
+./configure $OPTIONS
