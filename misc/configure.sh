7841c83e5f6765a6617fc367f90b849ac539543a
HAYASHI Kentaro <kenhys@gmail.com>
2015-04-05 00:30:04 +0900

    Add more USE options

--- misc/configure.sh
+++ misc/configure.sh
@@ -6,6 +6,12 @@ fi
 if [ -n "$USE_GTKHTML" ]; then
     OPTIONS="$OPTIONS --enable-gtkhtml"
 fi
+if [ -n "$USE_LIBNOTIFY" ]; then
+    OPTIONS="$OPTIONS --enable-libnotify"
+fi
+if [ -n "$USE_LIBAPPINDICATOR" ]; then
+    OPTIONS="$OPTIONS --enable-libappindicator"
+fi
 if [ -n "$USE_DEBUG" ]; then
     OPTIONS="$OPTIONS --enable-debug"
 fi
